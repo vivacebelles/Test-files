9b0bec41087c354c97cafb80b99fee25875894a7
Annabelle Lolinco <alolinco@iastate.edu>
2018-01-19 16:56:32 -0600

    adjust lines of code; now can work

--- test1.py
+++ test1.py
@@ -1,21 +1,11 @@
-print "Hello World!"
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
- 
+print "Hello World!";
+
+# Let's try something more difficult!
+f = 0
+print f;
+
+# redeclaring the variable
+f = "abc"
+print f;
+
+exit ()                                                                               
