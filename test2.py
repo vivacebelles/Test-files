d3308929600c09492f2e3f3f0b095f5c2733c647
Annabelle Lolinco <alolinco@iastate.edu>
2018-01-22 12:00:16 -0600

    add python functions to file

--- test2.py
+++ test2.py
@@ -1,2 +1,12 @@
+#working with functions
 
-print ("Hello world!");
+def func1():
+	print "I am a function";
+
+func1 ();
+print func1();
+
+def func2 (arg1, arg2):
+	print arg1, arg2;
+
+print func2 (10, 20);
