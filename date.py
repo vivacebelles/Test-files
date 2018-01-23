b6fa13979c348d83adc5f424dd94d5fd25f282e6
Annabelle Lolinco <alolinco@iastate.edu>
2018-01-23 09:29:02 -0600

    del 'def main()' in py code was unneccessary

--- date.py
+++ date.py
@@ -3,6 +3,5 @@ from datetime import date
 from datetime import time
 from datetime import datetime
 
-def main():
-	today = date.today()
-	print "Today's date is", today;
+today = date.today()
+print "Today's date is", today;
