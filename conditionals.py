79605210cefe0582f9f0bd4ebe96def243a14d06
Annabelle Lolinco <alolinco@iastate.edu>
2018-01-26 13:08:19 -0600

    new files from Py3 Essential Traning batch add

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+a, b = 0,1
+if a < b:
+	print ("a is ({}) is less than  b ({})".format(a,b))
+else:
+	print ("a is ({}) is not less than b({})".format(a,b))
+
+x, y = 5,3
+print ("foo" if x < y else "bar");
+print ("x is ({}) is less than y+b({})".format(x,y+b) if x <  y+b else "nah man") 
+print ("You did it!");
---
+++ generator.py
@@ -0,0 +1,18 @@
+#creates iterations
+
+def isprime(n):
+	if n == 1:
+		return False
+	for x in range(2,n):
+		if n % x == 0:
+			return False
+	else:
+		return True
+
+def primes(n = 1):
+	while(True):
+		if isprime(n): yield n
+		n += 1
+for n in primes():
+	if n > 50: break
+	print (n) 
