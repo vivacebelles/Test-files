e9f6d2c1c8e66feead7c12ea73f228efbce6ce5d
Annabelle Lolinco <alolinco@iastate.edu>
2018-02-13 16:48:45 -0600

    work for 2/13/2018

--- calendars.py
+++ calendars.py
@@ -2,11 +2,11 @@ import calendar
 
 c = calendar.TextCalendar(calendar.MONDAY)
 str = c.formatmonth(2018, 2, 0, 0)
-print str;
+print(str);
 
 hc = calendar.HTMLCalendar(calendar.SUNDAY)
 str2 = hc.formatmonth(2013,2)
-print str2;
+print(str2);
 
 for i in c.itermonthdays(2018,2):
-	print i;
+	print(i);
